02ef581291f277223c142d17601513a50d27cb67
Lilyana Klecharova <lklecharova9@gmail.com>
2023-01-26 13:52:53 +0200

    Add files via upload

---
+++ DOM Introduction - Lab/Show more/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    // Make text appear/disappear on button click
+
+    document.getElementById("text").style.display = "inline";
+    document.querySelector("#text").style.display = "inline";
+    document.getElementById("more").style.display = "none";
+}
