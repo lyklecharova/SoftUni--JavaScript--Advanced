9aea8957c35f8803b9a1c3498a890b414ca09e50
Lilyana Klecharova <lklecharova9@gmail.com>
2023-02-03 20:22:55 +0200

    Delete DOM Introduction - Exercise/01. Subtraction directory

--- DOM Introduction - Exercise/01. Subtraction/subtract.js
+++
@@ -1,8 +0,0 @@
-function subtract() {
-    let firstNumber = document.getElementById("firstNumber").value;
-    let secondNumber = document.getElementById("secondNumber").value;
-    let result = document.getElementById("result");
-
-    result.textContent = Number(firstNumber) - Number(secondNumber);
-
-}
