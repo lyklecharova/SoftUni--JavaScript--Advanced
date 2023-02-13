e909aa9484dc8bd322e8673b4b912fd3e29ef766
Lilyana Klecharova <lklecharova9@gmail.com>
2023-02-13 13:55:51 +0200

    Delete 01. List-Of-Items directory

--- 01. List-Of-Items/app.js
+++
@@ -1,9 +0,0 @@
-function addItem() {
-    let ulLiElement = document.getElementById('items');
-    let inputTypeText = document.getElementById('newItemText');
-
-    let liElement = document.createElement('li');
-    liElement.textContent = inputTypeText.value;
-    ulLiElement.appendChild(liElement);
-
-}
