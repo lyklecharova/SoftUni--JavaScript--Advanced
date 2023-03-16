a3e461c59b41d922cc324e81a50382ebee1f0aaf
Lilyana Klecharova <lklecharova9@gmail.com>
2023-03-16 20:30:13 +0200

    Add files via upload

---
+++ Unit Testing and Error Handling - Lab/sumOfNumbers.js
@@ -0,0 +1,15 @@
+function sum(arr) {
+
+    let sum = 0;
+
+    for (let num of arr) {
+
+        sum += Number(num);
+
+    }
+
+    return sum;
+
+}
+
+module.exports = sum
---
+++ Unit Testing and Error Handling - Lab/sumOfNumbers.test.js
@@ -0,0 +1,13 @@
+describe("Sum of Numbers tests", () => {
+    it("Test 1: works with number array", () => {
+        expect(sum([1, 2, 3])).to.equal(6);
+    });
+
+    it("Test 2: works with number array as string", () => {
+        expect(sum(["1", "2", "3"])).to.equal(6);
+    });
+
+    it("Test 2: returns NaN, when 1 param is not number", () => {
+        expect(sum(["a", "2", "3"])).to.be.NaN;
+    });
+});
