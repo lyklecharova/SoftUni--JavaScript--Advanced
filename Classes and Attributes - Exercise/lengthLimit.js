312c3845f15d1b054b4ea163af9e710db29f78fc
Lilyana Klecharova <lklecharova9@gmail.com>
2023-04-02 17:04:03 +0300

    Add files via upload

---
+++ Classes and Attributes - Exercise/lengthLimit.js
@@ -0,0 +1,44 @@
+class Stringer{
+    constructor(innerString, innerLength){
+        this.innerString = innerString;
+        this.innerLength = innerLength;
+    };
+
+    increase(length){
+        this.innerLength += length;
+    };
+
+    decrease(length){
+        this.innerLength -= length;
+        if(this.innerLength <0){
+            this.innerLength = 0;
+        }
+    };
+
+    toString(){
+        if(this.innerLength > this.innerString){
+            return `${this.innerString.slice(0,this.innerLength)}   `;
+        }
+
+        if(this.innerLength === 0){
+            return `...`;
+        }
+        return this.innerString;
+    }
+}
+
+let test = new Stringer("Test", 5);
+
+console.log(test.toString()); // Test
+
+test.decrease(3);
+
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+
+console.log(test.toString()); // ...
+
+test.increase(4);
+
+console.log(test.toString()); // Test
